5da6a09e4f52ee29709c40c407d83fd2767c285d
yUnoming <yuknownme@gmail.com>
2026-06-19 02:57:35 +0900

    Add: スコア機能
    ## 概要
    - スタート位置から進んだ距離をスコアとして保存
    
    ## 実装内容
    - `GamelayManager.GameOver()`の第一引数としてプレイヤー座標を渡し、スコアとして保存
    
    ## 動作確認
    - プレイヤーの移動距離が正しくスコアとして保存されていること

--- ZigzagCube/Assets/Game/Scripts/Manager/Scene/GameplayManager.cs
+++ ZigzagCube/Assets/Game/Scripts/Manager/Scene/GameplayManager.cs
@@ -2,9 +2,19 @@
 
 public class GameplayManager : SceneManagerBase<GameplayManager>
 {
-    public void GameOver()
+    private ResultData resultData = new ResultData();
+
+    /// <summary>
+    /// ゲームオーバー処理    </summary>
+    /// <param name="playerPosition">
+    /// プレイヤー座標 </param>
+    public void GameOver(Vector3 playerPosition)
     {
-        // 他のゲームオーバー処理があれば記述
+        // 直近プレイのスコア保存
+        resultData.score = (int)playerPosition.z;   // スコア(スタート位置からの移動距離)
+        Debug.Log("スコア:" + resultData.score);
+
+        // リザルトへ遷移
         ChangeScene(SceneType.Result, false);
     }
 }

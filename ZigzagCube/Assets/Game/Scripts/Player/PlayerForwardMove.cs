66020670d0806d7e6e3542e1ecf063a2a0e85315
yUnoming <yuknownme@gmail.com>
2026-06-22 12:40:20 +0900

    Add: プレイヤーの移動速度上昇
    ## 概要
    - ゲーム進行による難易度上昇の一つとして、進んだ距離による移動速度の上昇を実装
    
    ## 実装内容
    - プレイヤーの移動コンポーネントを「前方」「横移動」2種のコンポーネントに分離
    - 前方の移動速度を一定距離の移動で上昇
    - 横移動の再移動までの時間を一定距離の移動で短縮
    
    ## 動作確認
    - 一定距離の移動によって各方向の移動速度が上昇すること

---
+++ ZigzagCube/Assets/Game/Scripts/Player/PlayerForwardMove.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerForwardMove : BehaviorBase
+{
+    [SerializeField, Tooltip("移動速度")]
+    private float baseSpeed = 1f;
+    [SerializeField, Tooltip("速度の上昇量")]
+    private float speedIncreaseAmount = 1f;
+    [SerializeField, Tooltip("速度が上昇する距離間隔")]
+    private float speedIncreasePerDistance = 100f;
+    [Header("=====")]
+    [SerializeField]
+    private Rigidbody rb;
+
+    /// <summary>
+    /// 現在の速度    </summary>
+    private float speed;
+    /// <summary>
+    /// 最後に速度が上昇した距離    </summary>
+    private float lastSpeedIncreaseDirection;
+
+    public override void Initialize()
+    {
+        speed = baseSpeed;
+        base.Initialize();
+    }
+    public override void Execute(InputData inputData)
+    {
+        // 一定距離の移動で速度上昇
+        if (transform.position.z - lastSpeedIncreaseDirection >= speedIncreasePerDistance)
+        {
+            speed += speedIncreaseAmount;
+            Debug.Log($"現在の移動速度:{speed}");
+            lastSpeedIncreaseDirection = transform.position.z;
+        }
+        // 前方移動
+        rb.linearVelocity = new Vector3(0, rb.linearVelocity.y, speed);
+    }
+}
---
+++ ZigzagCube/Assets/Game/Scripts/Player/PlayerLateralMove.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class PlayerLateralMove : BehaviorBase
+{
+    [SerializeField, Tooltip("移動速度")]
+    private float baseSpeed = 1f;
+    [SerializeField, Tooltip("再移動までの時間")]
+    private float baseCoolTime = 0.65f;
+    [SerializeField, Tooltip("再移動までの時間の減少量")]
+    private float coolTimeDecreaseAmount = 1f;
+    [SerializeField, Tooltip("再移動までの時間が減少する距離間隔")]
+    private float coolTimeDecreasePerDistance = 100f;
+
+    /// <summary>
+    /// 速度    </summary>
+    private float speed;
+    /// <summary>
+    /// 移動方向    </summary>
+    private float direction = 1f;
+    /// <summary>
+    /// 再移動までの時間    </summary>
+    private float coolTime;
+    /// <summary>
+    /// 経過時間    </summary>
+    private float elapsedTime;
+    /// <summary>
+    /// 最後に速度が上昇した距離    </summary>
+    private float lastSpeedIncreaseDirection;
+
+    public override void Initialize()
+    {
+        speed = baseSpeed;
+        coolTime = baseCoolTime;
+        base.Initialize();
+    }
+    public override void Execute(InputData inputData)
+    {
+        // 一定距離の移動で再移動までの時間短縮
+        if (transform.position.z - lastSpeedIncreaseDirection >= coolTimeDecreasePerDistance)
+        {
+            coolTime -= coolTimeDecreaseAmount;
+            Debug.Log($"再移動までの時間:{coolTime}");
+            lastSpeedIncreaseDirection = transform.position.z;
+        }
+        // 一定時間経過後に左右移動
+        if (elapsedTime >= coolTime)
+        {
+            transform.position += new Vector3(speed * direction, 0);
+            elapsedTime = 0;    // 経過時間のリセット
+        }
+        else
+        {
+            // タッチで左右移動の方向切り替え
+            if (inputData.isTouch)
+            {
+                Debug.Log("タッチされた");
+                direction *= -1f;
+            }
+            elapsedTime += Time.deltaTime;  // 経過時間の計測
+        }
+    }
+}

8160f708f6152aab5b62f9de9b3ea994a8c00e0c
yUnoming <yuknownme@gmail.com>
2026-06-28 21:40:30 +0900

    Add: 壁の除外
    ## 概要
    - ゲーム進行によるギミックとして、一定スコア間隔で壁を除外する機能の追加
    
    ## 実装内容
    - リスト内の要素をシャッフルするユーティリティを作成
    - 一定スコア間隔でチャンク内の壁オブジェクトから一定数を除外する処理を追加
    
    ## 動作確認
    - 一定スコア到達時に壁オブジェクトが除外されること
    - 壁オブジェクトが最大除外数を越えて除外されないこと
    - チャンク生成ごとに除外される壁オブジェクトが変化していること

---
+++ ZigzagCube/Assets/Game/Scripts/Terrain/WallSpawner.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSpawner : MonoBehaviour
+{
+    [SerializeField, Tooltip("最大減少数")]
+    private int maxDecreaseCount = 20;
+    [SerializeField, Tooltip("生成数の減少量")]
+    private int spawnDecreaseAmount = 1;
+    [SerializeField, Tooltip("生成数が減少するスコア間隔")]
+    private float spawnDecreasePerScore = 500;
+    [Header("=====")]
+    [SerializeField] private List<GameObject> walls;
+
+    /// <summary>
+    /// 最後に生成数が増加した際のスコア値   </summary>
+    private float lastSpawnDecreaseScore;
+    /// <summary>
+    /// 減少回数    </summary>
+    private int decreaseCount = 0;
+    /// <summary>
+    /// 非表示の壁    </summary>
+    private List<GameObject> invisibleWalls = new List<GameObject>();
+
+    private void Update()
+    {
+        // 一定のスコア間隔で生成数を増加
+        int currentScore = GameplayManager.Instance.Score;
+        if (currentScore - lastSpawnDecreaseScore >= spawnDecreasePerScore)
+        {
+            decreaseCount += spawnDecreaseAmount;
+            decreaseCount = Mathf.Min(decreaseCount, maxDecreaseCount);  // 最大数に抑える
+
+            lastSpawnDecreaseScore = currentScore;
+        }
+    }
+
+    /// <summary>
+    /// 障害物の生成    </summary>
+    public void Spawn()
+    {
+        // 非表示させる必要がなければ早期リターン
+        if (decreaseCount == 0) return;
+
+        // 壁のリセット
+        foreach(GameObject wall in invisibleWalls) { wall.SetActive(true); }
+        invisibleWalls.Clear();
+        // 壁リストをシャッフル
+        CollectionUtility.Shuffle(walls);
+        // 壁リストを減少回数に応じて非表示に切り替え
+        for(int invisibleCount = 0; invisibleCount < decreaseCount; invisibleCount++)
+        {
+            walls[invisibleCount].SetActive(false);
+            invisibleWalls.Add(walls[invisibleCount]);
+        }
+    }
+}
---
+++ ZigzagCube/Assets/Game/Scripts/Utility/CollectionUtility.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class CollectionUtility
+{
+    /// <summary>
+    /// リスト内の要素をシャッフル    </summary>
+    public static void Shuffle<T>(List<T> list)
+    {
+        // リストの要素数のループ
+        for(int index = 0; index < list.Count; index++)
+        {
+            // リスト範囲内で乱数生成
+            int randNum = Random.Range(0, list.Count - index);
+            // 現在/乱数の要素番号にある要素の配置入れ替え
+            T tmp = list[index];
+            list[index] = list[randNum];
+            list[randNum] = tmp;
+        }
+    }
+}

74fdeee4565bd360cbeaa912aa26eddb59ff1057
yUnoming <yuknownme@gmail.com>
2026-06-29 14:39:03 +0900

    Update: ゲームカメラをプレイヤーから分離
    ## 概要
    - プレイヤーの挙動がそのままカメラへ反映されるのを防ぐため、ゲームカメラをプレイヤーから分離
    
    ## 実装内容
    - ゲームカメラをプレイヤーの子オブジェクトから分離
    - ゲームカメラをゲームシーンに配置
    - ゲームカメラにプレイヤー追従機能を追加
    
    ## 動作確認
    - ゲームカメラがプレイヤーの子オブジェクトではないこと
    - 分離後もゲームカメラがプレイヤーを正常に追従すること

---
+++ ZigzagCube/Assets/Game/Scripts/GameplayCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class GameplayCamera : MonoBehaviour
+{
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private GameObject player;
+
+    private void FixedUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}

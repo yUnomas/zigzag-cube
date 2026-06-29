7c590a3074816393ab55b77c589ef191028eb11e
yUnoming <yuknownme@gmail.com>
2026-06-29 13:19:27 +0900

    Update: 横方向からの壁衝突で跳ね返り
    ## 概要
    - 壁に当たるだけで確実に助かってしまう挙動を防ぐため、横方向から壁に衝突した場合のみ跳ね返るように変更
    
    ## 実装内容
    - 壁との衝突時、横方向からの衝突かどうかを判定
    - 横方向から衝突した場合のみ、跳ね返り処理を実行するように変更
    
    ## 動作確認
    - 横方向から壁に当たると跳ね返ること
    - 壁に空いた穴から落ちそうな際、前方の壁に当たっても跳ね返らないこと

--- ZigzagCube/Assets/Game/Scripts/Player/PlayerBounce.cs
+++ ZigzagCube/Assets/Game/Scripts/Player/PlayerBounce.cs
@@ -6,32 +6,28 @@ public class PlayerBounce : MonoBehaviour
     [SerializeField, Tooltip("ヒット対象のタグ一覧")]
     private List<string> hitTags = new List<string>();
 
-    private void Bounce()
-    {
-        GetComponent<PlayerMovement>().TriggerDirection();
-    }
-
     private void OnCollisionEnter(Collision collision)
     {
         // ヒット対象との衝突で跳ね返り
         foreach (string tag in hitTags)
         {
+            // 横から壁に当たった場合、跳ね返る
             if (collision.gameObject.tag == tag)
             {
-                Bounce();
+                Vector3 normal = collision.contacts[0].normal;
+                if(Mathf.Abs(normal.x) > 0.9f)
+                {
+                    Bounce();
+                }
                 break;
             }
         }
     }
-    private void OnTriggerEnter(Collider other)
+
+    /// <summary>
+    /// 跳ね返り処理    </summary>
+    private void Bounce()
     {
-        foreach (string tag in hitTags)
-        {
-            if (other.gameObject.tag == tag)
-            {
-                Bounce();
-                break;
-            }
-        }
+        GetComponent<PlayerMovement>().TriggerDirection();
     }
 }

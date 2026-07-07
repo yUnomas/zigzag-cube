ad8e81cb12eea8d3a33e8db882c7a81c7a22ff97
yUnoming <yuknownme@gmail.com>
2026-07-07 17:28:53 +0900

    Add: プレイヤーデータ
    ## 概要
    - 各プレイヤーの固有情報を管理するプレイヤーデータを追加
    
    ## 実装内容
    - プレイヤー名とIDを保持するプレイヤーデータを作成
    - 初回起動時にランダムなプレイヤー名を生成する処理を作成
    - プレイヤーデータのセーブ・ロード処理を追加
    
    ## 動作確認
    - プレイヤーデータのセーブ・ロードが正常に動作すること
    - 初回起動時にプレイヤー名とIDが生成されること
    - 生成済みのプレイヤー名とIDが、以降の起動時に勝手に変更されないこと

---
+++ ZigzagCube/Assets/Game/Scripts/Data/PlayerData.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerData
+{
+    public string name;
+    public string id;
+
+    /// <summary>
+    /// ランダムなプレイヤー名の生成    </summary>
+    private string GenerateRandomName()
+    {
+        string randomName = "";
+        string[] playerList = { "PLAYER", "ENDLESS", "RUNNER", "ZIGZAG", "CUBE" };
+        string[] symbolList = { "", "_", "#", "." };
+
+        randomName += playerList[Random.Range(0, playerList.Length)];
+        randomName += symbolList[Random.Range(0, symbolList.Length)];
+        randomName += Random.Range(0, 10000).ToString("D4");
+
+        Debug.Log(randomName);
+        return randomName;
+    }
+    public PlayerData()
+    {
+        name = GenerateRandomName();
+        id = System.Guid.NewGuid().ToString();
+    }
+}

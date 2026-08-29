a76515d9b049283018b2a30e147ef982da65fb4b
yUnoming <yuknownme@gmail.com>
2026-08-29 22:34:53 +0900

    Add: 大砲の発射エフェクト
    ## 概要
    - 大砲の発射を表現する煙エフェクトを追加
    
    ## 実装内容
    - 大砲の発射エフェクトを作成
    - 大砲の発射時にエフェクトを再生する処理を追加
    
    ## 動作確認
    - 大砲の発射時にエフェクトが正常に再生されること

---
+++ ZigzagCube/Assets/Game/Scripts/Effect/EffectController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class EffectController : MonoBehaviour
+{
+    [SerializeField, Tooltip("終了後の削除切り替え")]
+    private bool destroyOnFinished;
+    [Header("=====")]
+    [SerializeField] protected ParticleSystem effect;
+
+    protected virtual void Awake()
+    {
+        // 参照が未設定の場合は取得
+        if (effect == null) effect = GetComponent<ParticleSystem>();
+    }
+
+    /// <summary>
+    /// エフェクト削除 </summary>
+    /// <param name="t">
+    /// 削除までの期間 </param>
+    protected virtual void DestroyEffect(float delay = 0f)
+    {
+        Destroy(gameObject, delay);
+    }
+    /// <summary>
+    /// エフェクト再生    </summary>
+    public virtual void Play()
+    {
+        effect.Play();
+        if (destroyOnFinished)
+        {
+            float totalLifetime = effect.main.duration + effect.main.startLifetime.constantMax;
+            DestroyEffect(totalLifetime);
+        }
+    }
+}

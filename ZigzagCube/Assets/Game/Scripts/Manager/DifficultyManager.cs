626f8ba51bfd8a0f6482183ed306529c8b52281b
yUnoming <yuknownme@gmail.com>
2026-09-08 09:42:06 +0900

    Add: 移動距離による難易度上昇の管理
    ## 概要
    - 移動距離に応じて難易度を上昇させる管理処理を追加
    
    ## 実装内容
    - 難易度上昇を管理するスクリプトを追加
    - 一定距離を移動するごとに難易度を上昇させる処理を追加
    - 一定距離の移動時にプレイヤーの速度を上昇させる処理を、プレイヤーの移動スクリプトから難易度管理スクリプトへ移動
    
    ## 動作確認
    - 一定距離を移動するごとに難易度が上昇すること
    - 難易度上昇時にプレイヤーの速度が上昇すること

---
+++ ZigzagCube/Assets/Game/Scripts/Manager/DifficultyManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class DifficultyManager : MonoBehaviour
+{
+    [SerializeField, Tooltip("難易度上昇ごとのプレイヤーの速度上昇量")]
+    private float increasePlayerSpeedPerLevel = 0.25f;
+    [SerializeField, Tooltip("最初の難易度上昇に必要な距離")]
+    private float initialStepDistance = 50f;
+    [SerializeField, Tooltip("難易度の上昇に必要な移動距離。以降は倍増")]
+    private float distanceStepIncrease = 20f;
+    [SerializeField, Tooltip("必要距離の増加がストップする難易度レベル")]
+    private int maxStepLevel = 5;
+    [SerializeField, Tooltip("難易度の上昇がストップする難易度レベル")]
+    private int maxLevel = 10;
+    [Header("=====")]
+    [SerializeField] private PlayerMovement player;
+
+    /// <summary>
+    /// 現在の難易度レベル    </summary>
+    private int currentLevel = 0;
+    /// <summary>
+    /// 現在の難易度上昇に必要な距離    </summary>
+    private float currentStepDistance;
+    /// <summary>
+    /// 最後にの難易度上昇した際の距離    </summary>
+    private float lastIncreasedDistance;
+
+    private void Awake()
+    {
+        currentStepDistance = initialStepDistance;
+    }
+    private void Update()
+    {
+        if (currentLevel < maxLevel) return;
+
+        // 一定距離の移動で難易度を上昇
+        if (player.transform.position.z - lastIncreasedDistance >= currentStepDistance)
+        {
+            IncreaseDifficulty();
+        }
+    }
+
+    private void IncreaseDifficulty()
+    {
+        currentLevel++;
+        Debug.Log($"難易度が{currentLevel}に上昇しました");
+
+        // プレイヤーの速度上昇
+        player.AddSpeed(increasePlayerSpeedPerLevel);
+
+        // 次回必要な距離を設定
+        float maxStepDistance = initialStepDistance + (distanceStepIncrease * maxStepLevel);
+        currentStepDistance = Mathf.Min(currentStepDistance + distanceStepIncrease, maxStepDistance);
+        // 今回の難易度上昇時の距離を保持
+        lastIncreasedDistance = player.transform.position.z;
+    }
+}
